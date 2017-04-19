d8403314e456ac1aec1cbc85b8dc6bbf3fa77e1e
Hack Reactor Students <accounts@hackreactor.com>
2017-04-19 17:29:09 -0500

    automatic commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,11 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      options: {separator: ";"},
+      dist: {
+        src: ['public/client/**/*.js'],
+        dest: 'public/dist/<%= pkg.name %>.js'
+      }
     },
 
     mochaTest: {
@@ -21,6 +26,11 @@ module.exports = function(grunt) {
     },
 
     uglify: {
+      target: {
+        files: {
+          'public/dist/<%= pkg.name %>.min.js': ['client/**/*.js']
+        }
+      }
     },
 
     eslint: {
@@ -50,7 +60,11 @@ module.exports = function(grunt) {
     },
 
     shell: {
-      prodServer: {
+      commit: {
+        command: "git commit -am 'automatic commit'"
+      },
+      prodServer: { ///push to server
+        command: "git push live master"
       }
     },
   });
@@ -72,23 +86,25 @@ module.exports = function(grunt) {
   // Main grunt tasks
   ////////////////////////////////////////////////////
 
+  grunt.registerTask('push', ['shell']);
+
   grunt.registerTask('test', [
     'mochaTest'
   ]);
 
-  grunt.registerTask('build', [
-  ]);
+  grunt.registerTask('build', [ 'concat', 'uglify']);
 
   grunt.registerTask('upload', function(n) {
     if (grunt.option('prod')) {
       // add your production server task here
+      grunt.task.run(['build', 'push']);
     } else {
       grunt.task.run([ 'server-dev' ]);
     }
   });
 
   grunt.registerTask('deploy', [
-    // add your deploy tasks here
+    
   ]);
 
 
